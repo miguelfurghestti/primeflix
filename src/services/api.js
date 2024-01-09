f4d1eac25702cb6241fa7eea5873698250430946
Miguel Sampaio Furghestti <miguel.aghei@hotmail.com>
2024-01-09 17:38:28 -0300

    Consume api, style home page

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,10 @@
+import axios from "axios";
+
 //BASE DA API: https://api.themoviedb.org/3/
-//URL DA API: https://api.themoviedb.org/3/movie/11?api_key=ce887b5b04f11516853437fd193b9766
+//URL DA API: /movie/now_playing?api_key=ce887b5b04f11516853437fd193b9766&language=pt-BR
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+});
+
+export default api;
