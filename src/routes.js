a20948533372faf3326c14db78b61fc5805c192e
Miguel Sampaio Furghestti <miguel.aghei@hotmail.com>
2024-01-09 17:50:35 -0300

    Add not found page error 404

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Erro from "./pages/Erro";
 import Filme from "./pages/Filme";
 import Home from "./pages/Home";
 
-import Header from './components/Header';
+import Header from "./components/Header";
 
 function RoutesApp() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+
+        <Route path="*" element={<Erro />} />
       </Routes>
     </BrowserRouter>
   );
